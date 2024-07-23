b799b84eb04c48536d1a4308acf3c87312b3da2e
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-23 19:03:24 +0300

    Написал класс для исключения, уведомляющего о циклических зависимостях

---
+++ src/main/java/domain/exceptions/CircularRequireException.java
@@ -0,0 +1,13 @@
+package domain.exceptions;
+
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * Исключение для обозначения ситуаций, в которых возникло циклическое применение инструкции require
+ */
+public class CircularRequireException extends Throwable {
+    public CircularRequireException(List<Path> cycle) {
+        super("Обнаружена циклическая зависимость: " + cycle);
+    }
+}
