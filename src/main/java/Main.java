d75e1f6ef98f2d507f8c80c37f0c518250b00898
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-23 19:05:36 +0300

    Добавил код для получения пользовательской директории и передачи её в алгоритм

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,12 @@ import domain.FileLinker;
 
 public class Main {
     public static void main(String[] args) {
-        FileLinker linker = new FileLinker("test_folder");
         if (args.length < 1) {
             throw new IllegalArgumentException("Укажите путь к директории, в которой необходимо объединить файлы");
         }
+
+        String directoryPath = args[0];
+        FileLinker linker = new FileLinker(directoryPath);
         linker.link();
     }
 }
