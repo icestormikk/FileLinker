6504f5b64c17bb73ba0cc86575d70eeb967a83b1
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-23 19:05:04 +0300

    Написал код для обработки ситуации, когда пользователь не указал нужную директорию

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import domain.FileLinker;
 public class Main {
     public static void main(String[] args) {
         FileLinker linker = new FileLinker("test_folder");
+        if (args.length < 1) {
+            throw new IllegalArgumentException("Укажите путь к директории, в которой необходимо объединить файлы");
+        }
         linker.link();
     }
 }
